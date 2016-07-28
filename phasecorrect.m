37fae0f22623e5789b03214a4e12ff9b93fe1996
saurabhtauke <saurabh.talele@students.iiserpune.ac.in>
2016-07-28 18:48:51 +0530

    Update phasecorrect.m

--- phasecorrect.m
+++ phasecorrect.m
@@ -1,17 +1,16 @@
-
 % Tiny code for corercting the phase discontinuities in the phase grabbing From the Lockin.
 % The discontinuities arrise if the phase of our system exceeds the upper limit of arctan(Y/X) which is 90 degrees.
 
 
 
-x= importdata('testp.txt');
-
-p = x
+x= importdata('podata.txt');
+y= getfield(x,'data');
+p = y(:,3);                         % Third column is the Phase.
 for i = 1:200
     
-    if (p(i)< 0)
-        p(i)= p(i) + 20.000000
+    if (p(i) <  0.5)
+        p(i)= p(i) + 3.14;
     end
 end
-p
+p;
 plot (p)
