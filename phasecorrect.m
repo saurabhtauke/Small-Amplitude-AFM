aab8622c1f979ed5fc284fd6a7ef58f60fc44917
saurabhtauke <saurabh.talele@students.iiserpune.ac.in>
2016-07-29 10:30:10 +0530

    Update phasecorrect.m

--- phasecorrect.m
+++ phasecorrect.m
@@ -2,15 +2,30 @@
 % The discontinuities arrise if the phase of our system exceeds the upper limit of arctan(Y/X) which is 90 degrees.
 
 
+Int_sensitivity = 40;  	% in mv/A
+npoints = 150;	% uptil what point do we need to correct the phase data
+phasecutoff = -0.139; % the cutoff point below which the phase would be take to the positive side
 
-x= importdata('podata.txt');
-y= getfield(x,'data');
-p = y(:,3);                         % Third column is the Phase.
-for i = 1:200
+a= importdata('q.txt');
+b = getfield(a,'data');
+
+%plot (b(:,3)) % is the phase column in the consolidated data sheet
+%p = b(:,3);                         % Third column is the Phase.
+
+for i = 1:npoints
     
-    if (p(i) <  0.5)
+    if (p(i) < phasecutoff )
         p(i)= p(i) + 3.14;
     end
 end
-p;
-plot (p)
+
+%p;
+%plot (p)
+
+x= b(:,7);
+y= b(:,8);
+
+amplitude = b(:,2)*0.25;
+
+phione = acos(x./amplitude);
+phitwo = asin(y./amplitude);
