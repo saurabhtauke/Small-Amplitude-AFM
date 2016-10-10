4081e2ea49919ae287ba829e46a95a27bd422726
saurabhtauke <saurabh.talele@students.iiserpune.ac.in>
2016-10-10 14:12:27 +0530

    Update analysis.m

--- analysis.m
+++ analysis.m
@@ -47,9 +47,9 @@ for i=1:approach_count
     b(1,:) = [];
 end
 
-% for i = datacutoff:length(b)
-%     b(datacutoff,:) = [];
-% end
+for i = datacutoff:length(b)
+    b(datacutoff,:) = [];
+end
 
 %length(b)
 
@@ -64,22 +64,26 @@ phase = b(:,3);                        % Third column is the Phase.
 DC = b(:,4);
 
 offset_phase = (min(phase));
+new_phase = 30*(pi/180);
 
+n= zeros(length(phase),1);
+for i = 1:length(phase)
+    if phase(i)< 0
+        n(i)=-1;
+    else 
+        n(i)=1;
+    end
+end
 
-%n= zeros(length(phase),1);
-%for i = 1:length(phase)
-%    if phase(i)< 0
-%        n(i)=-1;
-%    else 
-%        n(i)=1;
-%    end
-%end
+true_x = (x);
+%true_x = (x)*cos(new_phase)/cos(offset_phase);
 
-% true_x = x;
- true_y = y - min(y);
+true_y = (y)- min(y);
 
- true_x = real(x/cos(offset_phase));
- %true_y =real(n.* sqrt((-1*(x.^2)*((tan(offset_phase))^2))+ (y.^2)));
+%  true_x = real(x/cos(offset_phase));
+%   true_y =real(sqrt((-1*(x.^2)*((tan(offset_phase))^2))+ (y.^2)));
+ %true_y = 
+%  %true_y = true_y- min(true_y);
 
 z_dist = (z_volt - min(z_volt) )* (scanner_calib * opamp_gain) ; 
 
@@ -88,15 +92,15 @@ z_dist = (z_volt - min(z_volt) )* (scanner_calib * opamp_gain) ;
 
 
 stiffness = double(cantilever_stiffness* ((free_amplitude./amplitude).*(cos(phase)) -1)); 
-stiffness = stiffness - min(stiffness);
+%stiffness = stiffness - min(stiffness);
 
-damping = double(cantilever_stiffness * (1.0) * ((free_amplitude./(amplitude.*(drive_frequency))) .* (sin((pi/2)-phase)))) ;
-damping = damping- min(damping);
+damping = double(cantilever_stiffness * (1.0) * ((free_amplitude./(amplitude.*(drive_frequency))) .* (sin((phase))))) ;
+%damping = damping- min(damping);
 
 
 
 stiffx = double((-1)*(((0.666*cantilever_stiffness * cantilever_length).*true_x./free_amplitude)- 0.333*density*10*area*cantilever_length*drive_frequency*drive_frequency));
-%stiffx = stiffx - min(stiffx);
+stiffx = stiffx - min(stiffx);
 
 lever_damping = 10e-006;
 dampy = double(0.666*cantilever_stiffness*cantilever_length.*true_y./(free_amplitude.*drive_frequency));%- (0.333*lever_damping*cantilever_length));
@@ -104,83 +108,68 @@ dampy = double(0.666*cantilever_stiffness*cantilever_length.*true_y./(free_ampli
 
 retardation_time = double(dampy./(stiffx));
 
+rsq = true_x.^2 + true_y.^2;
+asq = amplitude.^2;
 
 
 %% Plotting
 
 phase = phase .* (180/pi); %converted phase to degrees
 
-
-% hold on
-% yyaxis left
-% plot(z_dist,amplitude)
-% 
-% yyaxis right
-% plot(z_dist,phase)
-% 
-% % plot(z_dist,true_x)
-% % plot(z_dist,true_y)
-% hold off
-
-subplot(3,3,1)
+subplot(4,2,1)
 plot(z_dist,amplitude,'o-b')
 title('Amplitude')
 xlabel('Distance(nm)')  
 ylabel('Amplitude(Å)')
 
-
-subplot(3,3,2)
-plot(z_dist,true_x,'o-b')
-title('x')
-xlabel('Distance(nm)')
-ylabel('x')
-
-
-subplot(3,3,3)
-plot(z_dist,true_y,'o-b')
-title('y')
-xlabel('Distance(nm)')
-ylabel('y')
-
-
-subplot(3,3,4)
+subplot(4,2,2)
 plot(z_dist,phase,'o-b')
-title('Phase')
+title('Phase(Deg.)')
 xlabel('Distance(nm)')
 ylabel('Phase(degrees)')
 
 
-subplot(3,3,5)
+subplot(4,2,3)
+plot(z_dist,true_x,'o-b')
+title('X')
+xlabel('Distance(nm)')
+ylabel('X(Å)')
+
+
+subplot(4,2,4)
+plot(z_dist,true_y,'o-b')
+title('Y')
+xlabel('Distance(nm)')
+ylabel('Y(Å)')
+
+subplot(4,2,5)
+plot(z_dist,stiffx,'o-b')
+title('Stiffness by Altered Boundary Condition')
+xlabel('Distance(nm)')
+ylabel('Stiffness(N/m)')
+
+subplot(4,2,6)
+plot(z_dist,dampy,'o-b')
+title('Damping by Altered Boundary Condition')
+xlabel('Distance(nm)')
+ylabel('Damping(Kg/s)')
+
+subplot(4,2,7)
 plot(z_dist,stiffness,'o-b')
-title('Stiffness')
+title('Stiffness By Point-Mass Model')
 xlabel('Distance(nm)') 
 ylabel('Stiffness(N/m)')
 
 
-subplot(3,3,6)
+subplot(4,2,8)
 plot(z_dist,damping,'o-b')
-title('Damping')
+title('Damping by Point-Mass Model')
 xlabel('Distance(nm)')
-ylabel('Damping(Ns/m)')
+ylabel('Damping(Kg/s)')
 
 
-subplot(3,3,7)
-plot(z_dist,DC,'o-b')
-title('Retardation Time')
-xlabel('Distance(nm)')
-ylabel('Retardation Time(s)')
-
-
-subplot(3,3,8)
-plot(z_dist,stiffx,'o-b')
-title('stiffx')
-xlabel('Distance(nm)')
-ylabel('stiffx')
-
-
-subplot(3,3,9)
-plot(z_dist,dampy,'o-b')
-title('dampy')
-xlabel('Distance(nm)')
-ylabel('dampy')
-
+% subplot(4,2,7)
+% plot(z_dist,DC,'o-b')
+% title('Retardation Time')
+% xlabel('Distance(nm)')
+% ylabel('Retardation Time(s)'
